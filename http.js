c35b319bbf34ad61b4e2c526422288b30b084ddc
qumadev <adrian.mamani.dev@gmail.com>
2023-11-24 15:47:18 -0500

    funcional_normal

---
+++ http.js
@@ -0,0 +1,16 @@
+const http = require('node:http') // protocolo HTTP
+const desiredPort = process.env.PORT ?? 1234
+
+const processRequest = (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('Bienvenido a mi página web')
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
